a2c2fa4650b6dac47f00b86ee5d95a8dc8822fd7
Natix6 <maxonschreavemyhusband@gmail.com>
2024-07-21 12:15:40 +0300

    Update Robot.java

--- timedRobotPractice/src/main/java/frc/robot/Robot.java
+++ timedRobotPractice/src/main/java/frc/robot/Robot.java
@@ -4,11 +4,6 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -25,8 +20,7 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private Joystick driverJoystick;
-  private TalonSRX motor;
+  
 
 
   /**
@@ -38,8 +32,6 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    driverJoystick = new Joystick(0);
-    motor = new TalonSRX(1);
   }
 
   /**
@@ -89,21 +81,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-
-    if(driverJoystick.getRawButton(1)) {
-      motor.set(ControlMode.PercentOutput, 0.3);
-    }
-
-    else if(driverJoystick.getRawButton(2)) {
-      motor.set(ControlMode.PercentOutput, -0.3);
-
-    } else {
-      motor.set(ControlMode.PercentOutput, 0);
-    }
-
-
-  }
+  public void teleopPeriodic() {}
 
   /** This function is called once when the robot is disabled. */
   @Override
